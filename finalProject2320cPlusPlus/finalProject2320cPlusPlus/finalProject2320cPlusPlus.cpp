fbb31716ad2ed1eba0277aedde5dd29d7a8e24a2
Kevin Bell <bellKevin@pm.me>
2022-11-23 15:33:54 -0700

    Update finalProject2320cPlusPlus.cpp

--- finalProject2320cPlusPlus/finalProject2320cPlusPlus/finalProject2320cPlusPlus.cpp
+++ finalProject2320cPlusPlus/finalProject2320cPlusPlus/finalProject2320cPlusPlus.cpp
@@ -8,9 +8,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <stack>
-#include <iomanip>
 using namespace std;
 
 //Create a project that reads a text file that contains a string.
@@ -46,30 +44,24 @@ string substitution(string fileContents, int key) {
 	return substitutionString;
 }
 
-//function to split one string into two strings
-vector<string> splitString(string fileContents) {
-	vector<string> splitString;
-	string firstString;
-	string secondString;
+//function to split one string into two strings. one string for characters in even positions in the original string, and one string for characters in odd positions in the original string
+void splitString(string fileContents, string& evenString, string& oddString) {
 	for (int i = 0; i < fileContents.length(); i++) {
 		if (i % 2 == 0) {
-			firstString += fileContents[i];
+			evenString += fileContents[i];
 		}
 		else {
-			secondString += fileContents[i];
+			oddString += fileContents[i];
 		}
 	}
-	splitString.push_back(firstString);
-	splitString.push_back(secondString);
-	return splitString;
 }
 
-//function to merge two strings into one string interweaving the characters
-string mergeString(vector<string> splitString) {
+//function to merge two string into one string, interweaving them together
+string mergeString(string evenString, string oddString) {
 	string mergedString;
-	for (int i = 0; i < splitString[0].length(); i++) {
-		mergedString += splitString[0][i];
-		mergedString += splitString[1][i];
+	for (int i = 0; i < evenString.length(); i++) {
+		mergedString += evenString[i];
+		mergedString += oddString[i];
 	}
 	return mergedString;
 }
@@ -77,74 +69,71 @@ string mergeString(vector<string> splitString) {
 //function to reverse a string using a stack
 string reverseString(string fileContents) {
 	string reversedString;
-	stack<char> reverseStack;
+	stack<char> myStack;
 	for (int i = 0; i < fileContents.length(); i++) {
-		reverseStack.push(fileContents[i]);
+		myStack.push(fileContents[i]);
 	}
-	while (!reverseStack.empty()) {
-		reversedString += reverseStack.top();
-		reverseStack.pop();
+	while (!myStack.empty()) {
+		reversedString += myStack.top();
+		myStack.pop();
 	}
 	return reversedString;
 }
 
-int main()
-{
+//function to write a file
+void writeFile(string fileName, string fileContents) {
+	ofstream myfile(fileName);
+	if (myfile.is_open()) {
+		myfile << fileContents;
+		myfile.close();
+	}
+	else {
+		cout << "Unable to open file";
+	}
+}
+
+
+int main() {
 	cout << "Final Project by Kevin Bell\n\n";
+	//read a file
 	string fileContents = readFile("text.txt");
 	cout << "Original: " << fileContents << endl;
 	//Once you have read in the original text, you need to substitute each character by adding 3 to the ASCII value of the character.
 	string substitutionString = substitution(fileContents, 3);
 	//Next, split the substituted string into 2 strings, one for characters in even positions in the original string, and one for characters in odd positions in the original string.
-	vector<string> splitStringVector = splitString(substitutionString);
+	string evenString;
+	string oddString;
+	splitString(substitutionString, evenString, oddString);
 	//Then reverse each of those strings using a stack.
-	string firstStringReversed = reverseString(splitStringVector[0]);
-	string secondStringReversed = reverseString(splitStringVector[1]);
-	cout << "file 1: " << firstStringReversed << endl;
-	cout << "file 2: " << secondStringReversed << endl;
+	string reversedEvenString = reverseString(evenString);
+	string reversedOddString = reverseString(oddString);
+	cout << "file 1: " << reversedEvenString << endl;
+	cout << "file 2: " << reversedOddString << endl;
 	//secondStringReversed and firstStringReversed go into two separate output files. Do not close the files for the 2 output files after writing the even and odd strings to them
-	ofstream myfile1("file1.txt");
-	if (myfile1.is_open()) {
-		myfile1 << firstStringReversed;
-		myfile1.close();
-	}
-	else {
-		cout << "Unable to open file";
-	}
-	
-	ofstream myfile2("file2.txt");
-	if (myfile2.is_open()) {
-		myfile2 << secondStringReversed;
-		myfile2.close();
-	}
-	else {
-		cout << "Unable to open file";
-	}
+	writeFile("file1.txt", reversedEvenString);
+	writeFile("file2.txt", reversedOddString);
 	cout << "both files written\n\n";
 	cout << "Now reverse that process to decrypt\n\n";
 	cout << " original: " << fileContents << endl;
 	//Begin by reading the data from the 2 files created during the encryption process -- do not use the string variables used in that encryption process, make sure you read the data from files
 	string file1Contents = readFile("file1.txt");
 	string file2Contents = readFile("file2.txt");
-	//Next, reverse the strings in the files using a stack.
-	string file1ContentsReversed = reverseString(file1Contents);
-	string file2ContentsReversed = reverseString(file2Contents);
+	//Next, reverse the strings in the 2 files using a stack.
+	string reversedFile1Contents = reverseString(file1Contents);
+	string reversedFile2Contents = reverseString(file2Contents);
 	//substitute each character by subtracting 3 from the ASCII value of the character.
-	string file1ContentsSubstituted = substitution(file1ContentsReversed, -3);
-	string file2ContentsSubstituted = substitution(file2ContentsReversed, -3);
-	//Merge the two strings into one string interweaving the characters.
-	vector<string> mergeStringVector;
-	mergeStringVector.push_back(file1ContentsSubstituted);
-	mergeStringVector.push_back(file2ContentsSubstituted);
-	string mergedString = mergeString(mergeStringVector);
-	cout << "decrypted: " << mergedString << endl;
+	string decryptedSubstitutionStringFile1 = substitution(reversedFile1Contents, -3);
+	string decryptedSubstitutionStringFile2 = substitution(reversedFile2Contents, -3);
+	//merge the 2 strings into one string, interweaving them together.
+	string decryptedMergedString = mergeString(decryptedSubstitutionStringFile1, decryptedSubstitutionStringFile2);
+	cout << "decrypted: " << decryptedMergedString << endl;
 	//Check if the decrypted string matches the original string.
-	if (fileContents == mergedString) {
-		cout << "The decrypted string matches the original string\n";
+	if (decryptedMergedString == fileContents) {
+		cout << "Decrypted and Original strings match!\n";
 	}
 	else {
-		cout << "The decrypted string does not match the original string\n";
-	}	
+		cout << "The strings do not match.\n";
+	}
 	system("pause");
 	return 0;
-} //end main function
+}
